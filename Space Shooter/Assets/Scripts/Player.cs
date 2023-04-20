b7f74dff6ff7027e82d07563ff0f6f4075bbebd7
Sebastian <sebastiankristensen1@gmail.com>
2023-04-21 00:44:10 +0200

    Added enemies
    Added Spawn routine for the enemy
    Added a container for the enemy
    Added health to player
    Added destroy function to enemy, laser and player

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -6,19 +6,27 @@ public class Player : MonoBehaviour
     [SerializeField] private GameObject _laserPrefab;
     [SerializeField] private float _fireRate;
     [SerializeField] private float _canFire;
+    [SerializeField] private int _lives;
 
-    private Camera mainCamera;
+    private SpawnManager _spawnManager;
+
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
-        mainCamera = Camera.main;
+
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is null");
+        }
     }
 
     void Update()
     {
         Movement();
         KeepPlayerOnScreen();
-        
+
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
             ShootLaser();
@@ -55,5 +63,16 @@ public class Player : MonoBehaviour
     {
         _canFire = Time.time + _fireRate;
         Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+    }
+
+    public void Damage()
+    {
+        _lives--;
+
+        if (_lives < 1)
+        {
+            _spawnManager.OnPlayerDeath();
+            Destroy(this.gameObject);
+        }
     }
 }
